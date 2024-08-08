498cedc9d02072440053b64f4331a71943007b30
Hummara Bashir <bashirh@state.gov>
2024-08-08 15:45:59 -0400

    release

--- android/app/src/main/java/com/readaroo/MainActivity.kt
+++ android/app/src/main/java/com/readaroo/MainActivity.kt
@@ -11,15 +11,15 @@ import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
      override fun onCreate(savedInstanceState: Bundle?) {
-    SplashScreen.show(this);  // add this
-    super.onCreate(null)
+    SplashScreen.show(this, true);  // add this
+    super.onCreate(savedInstanceState)
   }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "readaroo"
+  override fun getMainComponentName(): String = "ReadAroo"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
